74e6833123cac9ffad8a875350007b57ed809a3a
ijaamm <ijammusyaf@gmail.com>
2024-12-16 21:49:22 +0700

    untuk mengelola data mahasiswa

--- app/src/main/java/com/example/praktikum9/repository/RepositoryMhs.kt
+++ app/src/main/java/com/example/praktikum9/repository/RepositoryMhs.kt
@@ -1,7 +1,21 @@
 package com.example.praktikum9.repository
 
 import com.example.praktikum9.Data.entity.Mahasiswa
+import kotlinx.coroutines.flow.Flow
 
 interface RepositoryMhs {
     suspend fun insertMhs(mahasiswa: Mahasiswa)
+
+    //getAllMhs
+    fun getAllMhs(): Flow<List<Mahasiswa>>
+
+    //getMhs
+    fun getMhs(nim: String): Flow<Mahasiswa>
+
+    //deleteMhs
+    suspend fun deleteMhs(mahasiswa: Mahasiswa)
+
+    //updateMhs
+    suspend fun updateMhs(mahasiswa: Mahasiswa)
+
 }
