54b1fe49cf81dacf1bd026af2788904e3af4be26
ijaamm <ijammusyaf@gmail.com>
2024-12-16 21:53:04 +0700

    untuk route ke home, detail dan update

--- app/src/main/java/com/example/praktikum9/ui/navigation/AlamatNavigasi.kt
+++ app/src/main/java/com/example/praktikum9/ui/navigation/AlamatNavigasi.kt
@@ -2,4 +2,20 @@ package com.example.praktikum9.ui.navigation
 
 interface AlamatNavigasi {
     val route: String
+
+    object DestinasiHome : AlamatNavigasi {
+        override val route = "home"
+    }
+    object DestinasiDetail : AlamatNavigasi {
+        override val route = "detail"
+        const val NIM = "nim"
+        val routeWithArgs = "$route/{$NIM}"
+    }
+
+    object DestinasiUpdate : AlamatNavigasi {
+        override val route = "update"
+        const val NIM = "nim"
+        val routeWithArgs = "$route/{$NIM}"
+    }
+
 }
