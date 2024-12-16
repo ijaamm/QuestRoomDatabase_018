c204337323d34ba6eb8aaa8273e3f3d27828fc33
ijaamm <ijammusyaf@gmail.com>
2024-12-16 21:48:35 +0700

    untuk mendukung operasi yang diperlukan

--- app/src/main/java/com/example/praktikum9/Data/dao/MahasiswaDao.kt
+++ app/src/main/java/com/example/praktikum9/Data/dao/MahasiswaDao.kt
@@ -1,11 +1,32 @@
 package com.example.praktikum9.Data.dao
 
 import androidx.room.Dao
+import androidx.room.Delete
 import androidx.room.Insert
+import androidx.room.Query
+import androidx.room.Update
 import com.example.praktikum9.Data.entity.Mahasiswa
+import kotlinx.coroutines.flow.Flow
 
 @Dao
 interface MahasiswaDao {
     @Insert
     suspend fun insertMahasiswa(mahasiswa: Mahasiswa)
+
+    //getAllMahasiswa
+    @Query("SELECT * FROM mahasiswa ORDER BY nama ASC")
+    fun getAllMahasiswa(): Flow<List<Mahasiswa>>
+
+    //getMahasiswa
+    @Query("SELECT * FROM mahasiswa WHERE nim = :nim")
+    fun getMahasiswa(nim: String): Flow<Mahasiswa>
+
+    //deleteMahasiswa
+    @Delete
+    suspend fun deleteMahasiswa(mahasiswa: Mahasiswa)
+
+    //updateMahasiswa
+    @Update
+    suspend fun updateMahasiswa(mahasiswa: Mahasiswa)
+
 }
