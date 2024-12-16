9518b1cc5f3094a7edf077697c4a472be91b195d
ijaamm <ijammusyaf@gmail.com>
2024-12-16 23:13:27 +0700

    menambah initializer untuk terhubumg ke semua view model

--- app/src/main/java/com/example/praktikum9/viewmodel/PenyediaViewModel.kt
+++ app/src/main/java/com/example/praktikum9/viewmodel/PenyediaViewModel.kt
@@ -1,6 +1,7 @@
 package com.example.praktikum9.viewmodel
 
 import androidx.lifecycle.ViewModelProvider
+import androidx.lifecycle.createSavedStateHandle
 import androidx.lifecycle.viewmodel.CreationExtras
 import androidx.lifecycle.viewmodel.initializer
 import androidx.lifecycle.viewmodel.viewModelFactory
@@ -9,7 +10,28 @@ import com.example.praktikum9.KrsApp
 object PenyediaViewModel{
     val Factory = viewModelFactory {
         initializer {
-            MahasiswaViewModel(krsApp().containerApp.repositoryMhs
+            MahasiswaViewModel(
+                krsApp().containerApp.repositoryMhs
+            )
+        }
+
+        initializer {
+            HomeMhsViewModel(
+                krsApp().containerApp.repositoryMhs
+            )
+        }
+
+        initializer {
+            DetailMhsViewModel(
+                createSavedStateHandle(),
+                krsApp().containerApp.repositoryMhs
+            )
+        }
+
+        initializer {
+            UpdateMhsViewModel(
+                 createSavedStateHandle(),
+                 krsApp().containerApp.repositoryMhs
             )
         }
     }
